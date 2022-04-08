aa3cef3479095ab5af76e4013125de7d1652c5e4
gilmichael <gilmichael@regalado.com.ph>
2022-04-08 19:04:40 +0800

    chore(ci): add storybook.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,30 @@
 module.exports = {
-    extends: [
-        'airbnb-typescript',
-        'airbnb/hooks',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:jest/recommended',
-        'plugin:prettier/recommended'
-    ],
-    plugins: ['react', '@typescript-eslint', 'jest'],
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
+  extends: ["airbnb-typescript", "airbnb/hooks", "plugin:@typescript-eslint/recommended", "plugin:jest/recommended", "plugin:prettier/recommended", "plugin:storybook/recommended"],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        project: './tsconfig.json',
-    },
-    rules: {
-        "import/extensions": "off",
-        "import/no-extraneous-dependencies": "off",
-        'linebreak-style': 'off',
-        'prettier/prettier': [
-            'error',
-            {
-                endOfLine: 'auto',
-            },
-        ],
-    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": "off",
+    'linebreak-style': 'off',
+    'prettier/prettier': ['error', {
+      endOfLine: 'auto'
+    }]
+  }
 };
---
+++ src/stories/Glass.stories.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { GlassWidget } from "../components/GlassWidget";
+import { Glass } from "../lib/Glass";
+import { Portion } from "../lib/Portion";
+import { SimEngine } from "../lib/SimEngine";
+import { Stage, Layer } from "react-konva";
+
+const engine = new SimEngine();
+const root: Glass = engine.getGlass();
+root.pour(new Portion(250, 0));
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/Glass",
+  component: GlassWidget,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    glass: {
+      name: "glass",
+      defaultValue: root,
+      description: "This is the Glass abstraction used for rendering.",
+      control: {
+        type: null,
+      },
+    },
+    onSelect: {
+      name: "onSelect",
+      defaultValue: () => {},
+      description: "This is triggered when the the glass icon is selected.",
+      control: {
+        type: null,
+      },
+    },
+  },
+} as ComponentMeta<typeof GlassWidget>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof GlassWidget> = (args) => (
+  <Stage width={500} height={500}>
+    <Layer>
+      <GlassWidget {...args} />
+    </Layer>
+  </Stage>
+);
+
+export const Unselected = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Unselected.args = {
+  x: 20,
+  y: 20,
+  height: 50,
+  width: 30,
+  fontSize: 8,
+  isSelected: false,
+};
+
+export const Selected = Template.bind({});
+Selected.args = {
+  x: 20,
+  y: 20,
+  height: 50,
+  width: 30,
+  fontSize: 8,
+  isSelected: true,
+};
